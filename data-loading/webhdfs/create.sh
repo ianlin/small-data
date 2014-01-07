4ca48b74b437a06f7320a114d6c8d08f30a733ee
Ian Yu-Hsun Lin <nepenthes9@gmail.com>
2014-01-07 21:48:21 +0800

    remove '^M' by sed

--- data-loading/webhdfs/create.sh
+++ data-loading/webhdfs/create.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-redirect_url=$(curl -i -s -X PUT "http://localhost:50070/webhdfs/v1/user/cloudera/band_charts.from_webhdfs?user.name=cloudera&op=CREATE" | grep Location | awk '{print $NF}')
+# sed for removing ^M
+redirect_url=$(curl -i -s -X PUT "http://localhost:50070/webhdfs/v1/user/cloudera/band_charts.from_webhdfs?user.name=cloudera&op=CREATE" | grep Location | awk '{print $NF}' | sed 's/[[:cntrl:]]//')
+
 curl -i -X PUT -T band_charts "$redirect_url"

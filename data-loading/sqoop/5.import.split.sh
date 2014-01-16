f211dd396a3771ca4bf12161670d9819a9587dfc
Ian Yu-Hsun Lin <nepenthes9@gmail.com>
2014-01-16 09:10:45 +0800

    update sqoop script

--- data-loading/sqoop/5.import.split.sh
+++ data-loading/sqoop/5.import.split.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time sqoop import --connect "jdbc:mysql://localhost/hadoop" --username cloudera -P --target-dir user6 --query 'select id, name from user where id <= 3 AND $CONDITIONS' --split-by name -m 2
+sqoop import --connect "jdbc:mysql://localhost/hadoop" --table big_table --username cloudera -P --split-by name --target-dir big_table_1

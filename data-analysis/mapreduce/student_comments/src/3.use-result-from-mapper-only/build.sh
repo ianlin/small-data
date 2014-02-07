c63217d64129e6bf48c669da55b123315f074369
Ian Yu-Hsun Lin <nepenthes9@gmail.com>
2014-02-07 13:40:15 +0800

    update mr build script

--- data-analysis/mapreduce/student_comments/src/3.use-result-from-mapper-only/build.sh
+++ data-analysis/mapreduce/student_comments/src/3.use-result-from-mapper-only/build.sh
@@ -2,6 +2,10 @@
 
 javac -cp /usr/lib/hadoop/*:/usr/lib/hadoop/lib/*:/usr/lib/hadoop-mapreduce/*:/usr/lib/hadoop-mapreduce/lib/*:/usr/lib/hadoop-hdfs/*:/usr/lib/hadoop-hdfs/lib/*:/home/cloudera/small-data/data-analysis/lib/* -d jardir StudentComments.java
 
+if [ ! -e jardir ]; then
+    mkdir jardir
+fi
+
 if [[ $? -eq 0 ]]; then
     jar -cvf StudentComments.jar -C jardir/ .
 fi

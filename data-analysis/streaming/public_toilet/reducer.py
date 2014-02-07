4a2dc60407206f93b64c4ca243c58c83043e6b6b
Ian Yu-Hsun Lin <nepenthes9@gmail.com>
2014-02-07 09:59:23 +0800

    update streaming bug: should print the line in each iteration

--- data-analysis/streaming/public_toilet/reducer.py
+++ data-analysis/streaming/public_toilet/reducer.py
@@ -4,8 +4,8 @@
 from __future__ import division
 import sys
 
-def main():
-    result = {}
+def main(stdin):
+    """
     for line in sys.stdin:
         key, value = line.split('\t')
         if not isinstance(key, unicode):
@@ -16,6 +16,34 @@ def main():
         result[key]['total'] += 1
     for k, v in result.iteritems():
         print '\t'.join((k, str(v['total']), str(v['good']), str(float(v['good'] / v['total']))))
+    """
+
+    result = {}
+    stdin = stdin.readlines()
+    total_toilet = len(stdin)
+    lastkey = None
+    cnt = 0
+    for line in stdin:
+        key, value = line.strip().split('\t')
+        if not lastkey:
+            lastkey = key
+        if key not in result:
+            result[key] = {'total': 0, 'good': 0}
+        if key == lastkey:
+            cnt += 1
+            result[key]['good'] += int(value)
+            result[key]['total'] += 1
+        else:
+            result[lastkey]['good'] += int(value)
+            result[key]['total'] += 1
+            print '\t'.join((lastkey, str(result[lastkey]['total']), str(result[lastkey]['good']),\
+                             str(float(result[lastkey]['good'] / result[lastkey]['total']))))
+            cnt = 1
+        lastkey = key
+    if lastkey:
+        print '\t'.join((lastkey, str(result[lastkey]['total']), str(result[lastkey]['good']),\
+                         str(float(result[lastkey]['good'] / result[lastkey]['total']))))
 
 if __name__ == '__main__':
-    main()
+    stdin = sys.stdin
+    main(stdin)
--- data-analysis/streaming/public_toilet/run.sh
+++ data-analysis/streaming/public_toilet/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.4.0.jar -D mapred.reduce.tasks=0 -input input/public_toilet -output streaming_output/public_toilet -mapper mapper.py -file mapper.py
+hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.4.0.jar -input input/public_toilet -output streaming_output/public_toilet -mapper mapper.py -file mapper.py -reducer reducer.py -file reducer.py

47c5a3e48031a5761993ee4d9ccc31cd6838bc08
Ian Yu-Hsun Lin <nepenthes9@gmail.com>
2014-06-06 13:17:23 +0800

    MODIFY: minor change for correcting MR example

--- data-analysis/mapreduce/public_toilet/src/get-amout-and-proprotion-of-good-toilets-by-district/PublicToilet.java
+++ data-analysis/mapreduce/public_toilet/src/get-amout-and-proprotion-of-good-toilets-by-district/PublicToilet.java
@@ -9,10 +9,13 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -94,11 +97,16 @@ public class PublicToilet extends Configured implements Tool {
         }
         */
 
-        Job job = new Job(getConf(), "Public Toilet");
-        job.setJarByClass(getClass());
+        Configuration conf = new Configuration();
+
+        Job job = new Job(conf, "Public Toilet");
+        job.setJarByClass(PublicToilet.class);
 
         job.setMapperClass(PublicToiletMapper.class);
         job.setReducerClass(PublicToiletReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);

6770b88c4e9201fcad2252615dfea9581eaa51ab
Ian Yu-Hsun Lin <nepenthes9@gmail.com>
2014-06-09 01:56:00 +0800

    FIX: update streaming jar loction

--- data-analysis/streaming/public_toilet/run.sh
+++ data-analysis/streaming/public_toilet/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.4.0.jar -input input/public_toilet -output streaming_output/public_toilet -mapper mapper.py -file mapper.py -reducer reducer.py -file reducer.py
+hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming.jar -input input/public_toilet -output streaming_output/public_toilet -mapper mapper.py -file mapper.py -reducer reducer.py -file reducer.py
